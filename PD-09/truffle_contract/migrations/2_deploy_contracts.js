1f3501bd41dac41c56e98e3df8dfd06532841a7e
s-partoredjo <shane_partoredjo@hotmail.com>
2021-01-05 16:36:46 +0100

    remove

--- PD-09/truffle_contract/migrations/2_deploy_contracts.js
+++
@@ -1,7 +0,0 @@
-const game = artifacts.require("Gachagang");
-const { deployProxy } = require('@openzeppelin/truffle-upgrades');
-
-module.exports = async function(deployer) {
-	const instance = await deployProxy(game, { deployer, unsafeAllowCustomTypes: true });
-	console.log('Deployed', instance.address);
-};
